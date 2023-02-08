c542e73ee8c914ef1d3d4063e1cbb145aed844a1
willmindstrength <124820492+willmindstrength@users.noreply.github.com>
2023-02-08 22:09:19 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home() {
    
 
     
-        <hr>
+        
         <Header title="Welcome to my app!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
