b62c35ce2e3201faafdaf81ee291f357a168e052
willmindstrength <124820492+willmindstrength@users.noreply.github.com>
2023-02-09 01:23:16 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,27 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
+import React, { useState } from 'react';
+
+const sectors = [ "Prize 1", "Prize 2", "Prize 3", "Prize 4", "Prize 5", "Lose" ];
+
+const Wheel = () => {
+  const [rotation, setRotation] = useState(0);
+  const [prize, setPrize] = useState('');
+  const [isSpinning, setIsSpinning] = useState(false);
+
+  const spinWheel = () => {
+    setIsSpinning(true);
+    setRotation(rotation + 360 + Math.floor(Math.random() * 360));
+
+    setTimeout(() => {
+      setIsSpinning(false);
+      setPrize(sectors[Math.floor(Math.random() * sectors.length)]);
+    }, 1000);
+  };
+  
+export default Wheel;
+
 export default function Home() {
   return (
     <div className="container">
@@ -12,9 +33,15 @@ export default function Home() {
 
       <main>
     
-        <div id="wheel-container">
-          <div id="wheel"></div>
-          <button id="spin-button">Spin</button>
+        <div>
+          <div id="wheel" style={{ transform: `rotate(${rotation}deg)` }}>
+            <div id="inner-wheel">
+              <p id="prize-text">{prize}</p>
+            </div>
+          </div>
+          <button id="spin-button" onClick={spinWheel} disabled={isSpinning}>
+            {isSpinning ? 'Spinning...' : 'Spin'}
+          </button>
         </div>
    
 
