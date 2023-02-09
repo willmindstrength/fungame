eacc349d4aec27d7beebf3a0f082bb3fd9da076c
willmindstrength <124820492+willmindstrength@users.noreply.github.com>
2023-02-09 01:40:32 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,10 @@ import Footer from '@components/Footer'
 import React, { useState } from 'react';
 
 const sectors = [ "Prize 1", "Prize 2", "Prize 3", "Prize 4", "Prize 5", "Lose" ];
+  
 
-
+export default function Home() {
+  
   const [rotation, setRotation] = useState(0);
   const [prize, setPrize] = useState('');
   const [isSpinning, setIsSpinning] = useState(false);
@@ -21,8 +23,6 @@ const sectors = [ "Prize 1", "Prize 2", "Prize 3", "Prize 4", "Prize 5", "Lose" 
     }, 1000);
   };
   
-
-export default function Home() {
   return (
     <div className="container">
       <Head>
